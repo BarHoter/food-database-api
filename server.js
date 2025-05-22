c9b4646085ccb6454d44a30a0b8d208f7228838b
BarHoter <102954740+BarHoter@users.noreply.github.com>
2025-05-22 09:40:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require('express');  // ← זה חשוב!
 const cors = require('cors');
-const path = require('path');
-
+const fs = require('fs');
+const xlsx = require('xlsx');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
